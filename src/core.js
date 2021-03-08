921d98bca770375d7b7e2ac003f37875dd66c92e
caiouehara <caioueharamartins@gmail.com>
2021-03-08 15:41:41 -0300

    Core: Add AppendElements function, mainElement Variable

--- src/core.js
+++ src/core.js
@@ -1,21 +1,35 @@
 let core = {
+	mainElement: null,
 	allElements: {},
 	init: function(cssSelector){
-		let mainHTML = createMainElement()
-		this.create("main", mainHTML)
-		document.querySelector(cssSelector).appendChild(mainHTML);
+		this.mainElement = createMainElement();
+		document.querySelector(cssSelector).appendChild(this.mainElement);
 	},
 	create: function(elementNAME,elementHTML){
 	 	this.allElements[elementNAME] = elementHTML;
+	},
+	appendElements: function(){
+		let nodeToAppend = this.mainElement;
+		for(element in this.allElements){
+			this.mainElement.appendChild(this.allElements[element])
+		} 
 	}
 }
 
 function createMainElement(){
 	let mainHTML = document.createElement('div');
 	mainHTML.setAttribute('id', 'zh-main-element');
-	mainHTML.setAttribute('contenteditable', 'true');
 	return mainHTML;
 }
 
+function createParagraph(){
+	let p = document.createElement('p');
+	p.setAttribute('contenteditable', 'true');
+	return p;
+}
+
 core.init('body')
+core.create('p', createParagraph());
+core.appendElements();
+
 console.log(core.allElements)
