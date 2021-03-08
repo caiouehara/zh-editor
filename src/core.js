add26acc96826801d4c13e41d1a5d4d7b6cce0ed
caiouehara <caioueharamartins@gmail.com>
2021-03-08 16:32:59 -0300

    Core: Add Insert Function, Change mainElement -> container

--- src/core.js
+++ src/core.js
@@ -1,25 +1,25 @@
 let core = {
-	mainElement: null,
+	container: null,
 	allElements: {},
 	init: function(cssSelector){
-		this.mainElement = createMainElement();
-		document.querySelector(cssSelector).appendChild(this.mainElement);
+		this.container = createContainer();
+		document.querySelector(cssSelector).appendChild(this.container);
 	},
 	create: function(elementNAME,elementHTML){
 	 	this.allElements[elementNAME] = elementHTML;
 	},
 	appendElements: function(){
-		let nodeToAppend = this.mainElement;
+		let nodeToAppend = this.container;
 		for(element in this.allElements){
-			this.mainElement.appendChild(this.allElements[element])
+			this.container.appendChild(this.allElements[element])
 		} 
 	}
 }
 
-function createMainElement(){
-	let mainHTML = document.createElement('div');
-	mainHTML.setAttribute('id', 'zh-main-element');
-	return mainHTML;
+function createContainer(){
+	let containerHTML = document.createElement('div');
+	containerHTML.setAttribute('id', 'zh-container-element');
+	return containerHTML;
 }
 
 function createParagraph(){
@@ -28,8 +28,22 @@ function createParagraph(){
 	return p;
 }
 
+function insert(cssProp){
+	let sel = document.getSelection();
+	if(typeof sel != "undefined" && sel.rangeCount > 0){
+		let range = sel.getRangeAt(0);
+		console.log(range);
+		let span = document.createElement('span');
+		span.setAttribute('style', cssProp);
+		range.surroundContents(span);
+	}
+}
+
 core.init('body')
 core.create('p', createParagraph());
 core.appendElements();
 
 console.log(core.allElements)
+
+// Windows functions for Tests
+window.insert = insert;
